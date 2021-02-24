1b785e33cbfc21741e6d34e410c83aff496cee12
BartIntec <bart.hunerbein@intecbrussel.be>
2021-02-24 10:41:05 +0100

    custom buttonaction

--- calculationFunctions.js
+++ calculationFunctions.js
@@ -2,10 +2,24 @@
 console.log('the sum of 1 and 2 equals');
 let number1 =10;
 let number2 =20;
-console.log(sum(number1,number2));
-console.log(difference(number1,number2));
-console.log(multiplication(number1,number2));
-console.log(division(number1,number2));
+
+
+
+
+function custombuttonaction(mathematicalfunction){
+
+    //select the inputelement with id number1
+    let num1 = document.querySelector("#number1");
+    //select the inputelement with id number2
+    let num2 = document.querySelector("#number2");
+    //select the p element with id result
+    let result = document.querySelector("#result");
+
+
+    //change the text value of the result element in the DOM
+    result.innerText = mathematicalfunction(num1.value,num2.value);
+
+}
 
 function plusbuttonaction(){
 
@@ -20,10 +34,48 @@ function plusbuttonaction(){
     result.innerText = sum(num1.value,num2.value);
 
 }
+function minusbuttonaction(){
 
+    //select the inputelement with id number1
+    let num1 = document.querySelector("#number1");
+    //select the inputelement with id number2
+    let num2 = document.querySelector("#number2");
+    //select the p element with id result
+    let result = document.querySelector("#result");
+
+    //change the text value of the result element in the DOM
+    result.innerText = difference(num1.value,num2.value);
+
+}
+function multiplicationbuttonaction(){
+
+    //select the inputelement with id number1
+    let num1 = document.querySelector("#number1");
+    //select the inputelement with id number2
+    let num2 = document.querySelector("#number2");
+    //select the p element with id result
+    let result = document.querySelector("#result");
+
+    //change the text value of the result element in the DOM
+    result.innerText = multiplication(num1.value,num2.value);
+
+}
+function divisionbuttonaction(){
+
+    //select the inputelement with id number1
+    let num1 = document.querySelector("#number1");
+    //select the inputelement with id number2
+    let num2 = document.querySelector("#number2");
+    //select the p element with id result
+    let result = document.querySelector("#result");
+
+    //change the text value of the result element in the DOM
+    result.innerText = division(num1.value,num2.value);
+
+}
 
 //this function returns the sum of 2 numbers
-function sum(number1, number2){
+let sum = function sum(number1, number2){
 
     number1 = parseInt(number1);
     number2 = parseInt(number2);
@@ -31,7 +83,7 @@ function sum(number1, number2){
 }
 
 //this function returns the difference of 2 numbers
-function difference(number1, number2){
+let difference = function difference(number1, number2){
     
     number1 = parseInt(number1);
     number2 = parseInt(number2);
@@ -39,7 +91,7 @@ function difference(number1, number2){
 }
 
 //this function returns the multiplication of 2 numbers
-function multiplication(number1, number2){
+let multiplication = function multiplication(number1, number2){
     
     number1 = parseInt(number1);
     number2 = parseInt(number2);
@@ -47,7 +99,7 @@ function multiplication(number1, number2){
 }
 
 //this function returns the division of 2 numbers
-function division(number1, number2){
+let division = function division(number1, number2){
     
     number1 = parseInt(number1);
     number2 = parseInt(number2);
